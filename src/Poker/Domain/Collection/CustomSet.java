1a6d1a901d08e93457f8677846543dd793b747e1
rvlwldev <rvlwlasd77@gmail.com>
2023-06-16 20:50:36 +0900

    feat: 중복을 허용하지 않는 CustomSet 구현

---
+++ src/Poker/Domain/Collection/CustomSet.java
@@ -0,0 +1,26 @@
+package Poker.Domain.Collection;
+
+import java.util.ArrayList;
+
+public class CustomSet<E> extends ArrayList<E> {
+
+    public CustomSet() {
+        super();
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (!contains(e)) return super.add(e);
+
+        return false;
+    }
+
+    public void addAll(E[] arr) {
+        for (E e : arr) {
+            if (!contains(e)) {
+                super.add(e);
+            }
+        }
+    }
+
+}
